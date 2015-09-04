048323da8a41dc996a47638691d7a4d6e381ba02
Patrick Brady <pj.brady@comcast.net>
2015-09-03 20:58:38 -0700

    Changes the /n to \n in the print statement.

--- src/Tutorial/Main.java
+++ src/Tutorial/Main.java
@@ -2,8 +2,8 @@ package Tutorial;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Please visit the wiki on github.com for the tutorial on GitHub!/n" + 
-				"You can do this by clicking on tutorial, then on the right hand side, clicking/n" +
-				"on the bok  icon that should be the wiki. More information is there!!/n");
+		System.out.println("Please visit the wiki on github.com for the tutorial on GitHub!\n" + 
+				"You can do this by clicking on tutorial, then on the right hand side, clicking\n" +
+				"on the bok  icon that should be the wiki. More information is there!!\n");
 	}
 }
